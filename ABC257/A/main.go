794d1944d0b058d9ee62f3ff5064f04b685d3aa5
rayfiyo <108730891+rayfiyo@users.noreply.github.com>
2023-05-12 22:54:48 +0900

    new ans

---
+++ ABC257/A/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var N, X int32
+	fmt.Scanf("%d %d", &N, &X)
+	char := [...]string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+
+	if N < X {
+		ans := (X / N) - 1
+		fmt.Printf("%s\n", char[ans])
+	} else {
+		fmt.Printf("A\n")
+	}
+}
