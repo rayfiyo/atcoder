98482fa1d5d41e5fe001108614a6c005edca9d6e
rayfiyo <108730891+rayfiyo@users.noreply.github.com>
2023-05-13 18:44:14 +0900

    moved

--- ABC300/A/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/YosCiiCable/myAtCoder/archives/ABC300/A
-
-go 1.20
--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	"fmt"
+    "bufio"
+    "fmt"
+    "os"
 )
 
+// 対話形式だとTLEなるからね！
 func main() {
-	fmt.Println("ok")
+    r := bufio.NewReader(os.Stdin)
+    w := bufio.NewWriter(os.Stdout)
+    defer w.Flush()
+
+    var a, b, c int
+    var s string
+
+    fmt.Fscan(r, &a)
+    fmt.Fscan(r, &b, &c)
+    fmt.Fscan(r, &s)
+
+    fmt.Fprintln(w, a+b+c, s)
 }
