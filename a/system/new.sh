43c40b7109bc98a534d72357587035fce1f6d2c3
rayfiyo <108730891+rayfiyo@users.noreply.github.com>
2023-06-11 00:19:22 +0900

    ABC305

--- a/system/new.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir -p ./newContest/A
-echo "mkdir -p ./newContest/A"
-cp ./archives/baseCode.go ./newContest/A/main.go
-echo "cp ./archives/baseCode.go ./newContest/A/main.go"
--- main.go
+++ main.go
@@ -3,22 +3,33 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
+func memo() {
+	number := 123.4
+	// 四捨五入
+    fmt.Println(math.Round(number))
+	// 切り上げ
+	fmt.Println(math.Ceil(number))
+	// 切り捨て
+	fmt.Println(math.Floor(number))
+}
+
 // 入力最大値: 65536
 var sc = bufio.NewScanner(os.Stdin) // 高級、行区切り、トークン分割可能
 //var sr= bufio.NewReader(os.Stdin)	// 低レイヤ、区切り指定、バイナリ入力可能
 
-// strings型(スライス) 1行入力
+// strings型(スライス？) 1行入力
 func stringLine() string {
 	sc.Scan()
 	return sc.Text()
 }
 
-// int型スライス？ 1行入力
+// int型 1行入力
 func intSC() int {
 	sc.Scan()
 	inputInt, err := strconv.Atoi(sc.Text())
@@ -29,12 +40,16 @@ func intSC() int {
 }
 
 // int型スライス 1行入力 指定区切り
-func intLine() []int {
+func intSplit() []int {
 	slice := []int{}
-
 	sc.Scan()
-	text := strings.Split(sc.Text(), " ") // 空白区切り
-	// (sc.Text(), "ここが区切り文字だよ～")
+
+	// 区切り文字を指定する
+	// (sc.Text(), "ここで区切り文字指定だよ～")
+	text := strings.Split(sc.Text(), " ")
+
+	// 1文字ずつで区切る
+	//text := strings.SplitN(sc.Text(), "", len(sc.Text()))
 
 	for _, t := range text {
 		converted, err := strconv.Atoi(t)
@@ -50,6 +65,6 @@ func intLine() []int {
 func main() {
 	// 空白区切りならこれつける: sc.Split(bufio.ScanWords)
 
-	N := intLine()
+	N := intSplit()
 	fmt.Println(N[0])
 }
