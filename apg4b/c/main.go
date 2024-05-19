0bea11dbc586155bd6216729c82a1f3691c2332e
rayfiyo <rayfiyorex@gmail.com>
2024-05-19 22:06:00 +0900

    new apg4b C

---
+++ apg4b/c/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("いつも2525")
+	fmt.Println("AtCoderくん")
+}
+
+/*
+// Go で問題を再現すると以下のような感じ？
+
+func main() {
+	fmt.Println("いつも252)
+	fmt.Println("AtCoderくん"
+}
+*/
